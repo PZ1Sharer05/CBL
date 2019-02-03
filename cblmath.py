165fff4637a995895cf05191d75726d452db0f3b
Jeff Sharer <44546014+PZ1Sharer05@users.noreply.github.com>
2019-02-03 08:52:30 +0700

    Add files via upload

--- cblmath.py
+++ cblmath.py
@@ -35,11 +35,16 @@ def kilometerToMillimeter(valueOfKilometerToBeChangedToMm):
     print (valueOfMillimeterToBeChangedToKm * 1000000)
 
 #Circle
-def areaOfCircle(radius, piType):
-    if piType == "full_pi":
-        print (int(full_pi * radius * radius))
-    elif piType == "pi":
-        print (int(pi * radius * radius))
+def areaOfCircle(radius, circleType):
+    if circleType == "Full":
+        return 3.14 * radius * radius
+    elif circleType == "Semicircle":
+        return 0.5 * 3.14 * radius * radius
+    elif circleType == "Quarter circle":
+        return  0.25 * 3.14 * radius * radius
+    else:
+        print ("Error, not a circle type value, please choose between \n Full \n Semicircle \n and Quarter circle")
+
 def findCircumference(diameter):
     print (3.14 * diameter)
 
@@ -51,23 +56,33 @@ def findPerimeterOfSquare(side):
 
 #Rectangle
 def findAreaOfRectangle(length, breadth):
-    print (length * breadth)
+    return length * breadth
+
 def findPerimeterOfRectangle(length, breadth):
-    print ((length + breadth) * 2)
+    return (length + breadth) * 2
+
+
+
+
 #Triangle
-def findAreaOfTriangle(base, height):
-    print (base * height * 0.5)
-def findPerimeterOfEquilatralTriangle(side):
-    print (side * 3)
+def areaOfTriangle(base, height):
+    return base * height * 0.5
+def perimeterOfTriangle(side,nextSide,lastSide,type):
+    if type == "Equilateral":
+        return side * 3
+    elif type == "Isosceles":
+        return side + nextSide * 2
+    elif type == "Scalene":
+        return side + nextSide + lastSide
+    else:
+        print ("Error, not a triangle type, the only types are: \n Equilateral \n Isosceles \n and Scalene")
 
 #Volumes
-def findVolumeOfCube(sideOfCube):
-    print (sideOfCube ** 3)
-def findSurfaceAreaOfCube(sideOfCube):
-    print (sideOfCube * sideOfCube * 6)
-    
-def findVolumeOfCuboid(length, heightOfCuboid, breadthOfCuboid):
-    print (length * heightOfCuboid * breadthOfCuboid)
+def volumeOfCube(side):
+    return side ** 3
+
+def volumeOfCuboid(lengthOfCuboid, heightOfCuboid, breadthOfCuboid):
+    return lengthOfCuboid * heightOfCuboid * breadthOfCuboid
 
 def findVolumeOfCylinder(base, height):
      print (int(3.14 * base * base * height))
